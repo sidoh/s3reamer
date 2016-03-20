72801c5481ac56243535f5d1b3588cc93ff4ce8f
Christopher <cmullins@liveramp.com>
2016-03-19 17:36:36 -0700

    dont use a second inotify

--- directory_streamer.rb
+++ directory_streamer.rb
@@ -12,8 +12,9 @@ module S3reamer
   class DirectoryStreamer
     DEFAULT_OPTIONS = {
       pool_size: 4,
-      close_timeout: 30,
-      log_level: Logger::INFO
+      log_level: Logger::INFO,
+      reader_sleep_interval: 1,
+      reader_timeout: 10
     }
 
     attr_reader :options
@@ -25,21 +26,28 @@ module S3reamer
     end
 
     def stream_directory(directory:, bucket:)
-      open_files = ThreadSafe::Hash.new
+      file_statuses = ThreadSafe::Hash.new
       dir_watch = INotify::Notifier.new
       pool = Thread.pool(options[:pool_size])
 
-      dir_watch.watch(directory, :open, :recursive) do |e|
+      dir_watch.watch(directory, :open, :close, :recursive) do |e|
         filename = e.absolute_name
 
         next unless File.exists?(filename) and !File.directory?(filename)
 
         # If this is an "open" event, we should only process it if we haven't
         # already started on this file.
-        next if e.flags.include?(:open) and open_files.include?(filename)
+        next if e.flags.include?(:open) and file_statuses.include?(filename)
+
+        # If this is a "close" event, we should update the status to inform the
+        # worker thread
+        if e.flags.include?(:close) and file_statuses.include?(filename)
+          file_statuses[filename] = :close
+          next
+        end
 
         log.info "File opened: #{filename}"
-        open_files[filename] = true
+        file_statuses[filename] = :open
 
         pool.process {
           log.debug "Starting process for: #{filename}"
@@ -47,40 +55,26 @@ module S3reamer
           obj = bucket.object(filename[1..-1])
           io = S3reamer::S3WriteStream.new(obj)
 
-          log.debug "Initialized bucket"
+          log.debug "Initialized S3 streamer"
 
           open(filename) do |file|
-            log.debug "Setting up watch for: #{filename}"
-
             stopped = false
-            queue = INotify::Notifier.new
-            queue.watch(filename, :modify, :close) do |e2|
-              log.debug "Got event #{e2.flags.inspect} on #{filename}"
+            size = 0
+            start_time = Time.now
+
+            while file_statuses[filename] == :open
+              and (start_time + options[:reader_timeout]) > Time.now
+
               b = file.read
               io.write(b)
               log.debug "Read #{b.length} bytes"
-
-              if e2.flags.include?(:close)
-                queue.close
-                stopped = true
-              end
-            end
-
-            while !stopped
-              log.debug "Waiting for event on: #{filename}"
-              if IO.select([queue.to_io], [], [], options[:close_timeout])
-                queue.process
-              else
-                log.warn "Waited for too long for file to be modified/closed."
-                stopped = true
-              end
             end
 
             log.info "File closed. Completing S3 upload: #{filename}"
           end
 
           io.close
-          open_files.delete(filename)
+          file_statuses.delete(filename)
         }
       end
 
